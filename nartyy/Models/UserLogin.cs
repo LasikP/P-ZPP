77c64520986d4e208a091a21145562751eb361e3
Patryk Lasik <lasikpatryk@gmail.com>
2023-06-23 10:35:55 +0200

    zmiany

--- nartyy/Models/UserLogin.cs
+++ nartyy/Models/UserLogin.cs
@@ -1,8 +1,12 @@
-﻿namespace nartyy.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nartyy.Models
 {
     public class UserLogin
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+		[Required(ErrorMessage = "Pole Nazwa Użytkownika nie może być puste")]
+		public string Username { get; set; }
+		[Required(ErrorMessage = "Pole Hasło nie może być puste")]
+		public string Password { get; set; }
     }
 }
